84d9233c5007a85ecd200c610fbf9c65ff3891bd
cxzzx007 <2271816105@qq.com>
2019-01-15 09:13:53 +0800

    CheckHelper IsIPV4 IsIPV6 GetUrl

--- ToolHelper/CheckHelper.cs
+++ ToolHelper/CheckHelper.cs
@@ -90,5 +90,35 @@ namespace ToolHelper
         {
             return Regex.IsMatch(variable, @"^\\w+$");
         }
+        /// <summary>
+        /// 检验是否IPV4
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsIPV4(string ip)
+        {
+            string express = @"\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+            return Regex.IsMatch(ip, express);
+        }
+        /// <summary>
+        /// 检验是否IPV6
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        public static bool IsIPV6(string ip)
+        {
+            string express = @"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
+            return Regex.IsMatch(ip, express);
+        }
+        /// <summary>
+        /// 从文本中提取URL
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static string GetUrl(string content)
+        {
+            string express = @"^(f|ht){1}(tp|tps):\\/\\/([\\w-]+\\.)+[\\w-]+(\\/[\\w- ./?%&=]*)?";
+            return Regex.Match(content, express).Value;
+        }
     }
 }
